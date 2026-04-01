2140b8b7987aed241e295572013e9f657c3100b6
HoangPhuc2712 <hoangphuc0027@gmail.com>
2026-04-01 11:28:02 +0700

    commit changes

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@
 </template>
 
 <script setup lang="ts">
+import { watchEffect } from 'vue'
+import { useI18n } from 'vue-i18n'
 import Toast from 'primevue/toast'
 import ConfirmDialog from 'primevue/confirmdialog'
+
+const { t, locale } = useI18n()
+
+watchEffect(() => {
+  void locale.value
+  document.title = t('breadcrumb.root')
+})
 </script>
