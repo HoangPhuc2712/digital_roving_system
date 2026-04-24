21441b88e3844d7fd8edf4d600d8e7d79a605722
HoangPhuc2712 <hoangphuc0027@gmail.com>
2026-04-24 09:46:20 +0700

    Commit Changes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -24,7 +24,7 @@ export const routes = [
       ...areasRoutes,
       ...checkpointsRoutes,
       ...routesRoutes,
-      ...organizationsRoutes,
+      // ...organizationsRoutes,
       {
         path: 'tutorial',
         name: 'tutorial',
